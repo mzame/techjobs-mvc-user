552d4807685e09fb32189cf8c6c9b6e4d5235f65
mzame <madelyn.zamenski@mailfence.com>
2019-02-17 17:05:33 -0600

    Initial Commit

---
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -0,0 +1,50 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.JobData;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping("search")
+public class SearchController {
+
+    @RequestMapping(value = "")
+    public String search(Model model) {
+        model.addAttribute("columns", ListController.columnChoices);
+        return "search";
+    }
+
+    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String processSearch(Model model, @RequestParam String searchQuery,@RequestParam String searchTerm) {
+        ArrayList<HashMap<String, String>> jobs;
+        if (searchQuery.equals("all")) {
+
+            jobs = JobData.findByValue(searchTerm);
+
+        } else {
+
+            jobs = JobData.findByColumnAndValue(searchQuery, searchTerm);
+            }
+        model.addAttribute( "columns", ListController.columnChoices);
+        model.addAttribute("searchQuery", searchQuery);
+        model.addAttribute("searchTerm", searchTerm) ;
+        model.addAttribute("jobs", jobs);
+        return "results";
+
+
+
+    }
+
+    private void If(boolean all) {
+    }
+
+}
